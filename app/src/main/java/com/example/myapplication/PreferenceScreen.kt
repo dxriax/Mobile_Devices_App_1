d61e1ee90721bd6d3556d921e2b1fe32e1f25f70
dxriax <153857433+dxriax@users.noreply.github.com>
2024-05-02 20:12:10 +0200

    Basic Structure implemented for the second Activity / Add a Vote - Buttons still need work

--- app/src/main/java/com/example/myapplication/PreferenceScreen.kt
+++ app/src/main/java/com/example/myapplication/PreferenceScreen.kt
@@ -1,30 +1,20 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class PreferenceScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_preference_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        //Switch Screens by pressing "Add a Vote - Button"
-        val button = findViewById<Button>(R.id.btnAddVote)
-        button.setOnClickListener {
-            val intent = Intent(this, PreferenceScreen::class.java)
-            startActivity(intent)
         }
     }
 }
