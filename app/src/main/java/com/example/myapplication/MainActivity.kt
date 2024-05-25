ddca4a544490a92f7b1158c75573b8b5f8e801c8
dxriax <153857433+dxriax@users.noreply.github.com>
2024-05-25 19:14:55 +0200

    repaired activity switch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,54 +12,64 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var etNumOptions : EditText
-    private lateinit var etVotingOptions :EditText
+    private var etNumOptions : EditText? = null
+    private var etVotingOptions :EditText? = null
+    private var btnAddVote : Button? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Log.d("main activity", "on create exexute")
 
-        etNumOptions = findViewById(R.id.etNumOptions)
-        etVotingOptions = findViewById(R.id.etVotingOptions)
+        etNumOptions = findViewById<EditText>(R.id.etNumOptions) as EditText
+        etVotingOptions = findViewById<EditText>(R.id.etVotingOptions) as EditText
+        btnAddVote = findViewById<Button>(R.id.btnAddVote) as Button
 
 
-
-    etNumOptions.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
-        if (!hasFocus) {
-            // Hier wird der Code ausgeführt, wenn der Fokus vom EditText-Feld etNumOptions weggeht
-            checkInput()
+        // Set OnClickListener for btnAddVote
+        btnAddVote?.setOnClickListener {
+            onClick(it)
         }
+
+        // Set onFocusChangeListener for etNumOptions
+        etNumOptions?.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                checkInput()
+            }
+        }
+
     }
 
+    fun onClick(v: View) {
+        // Add a Vote - will fill missing string options and remove extra ones
+        //Switch Screens by pressing "Add a Vote - Button"
+        if (v == btnAddVote) {
+            var numOptions = etNumOptions!!.text.toString().toIntOrNull() ?: 3
+            var trimmedStringsOption =
+                etVotingOptions!!.text.toString().split(",").map { it.trim() }.toMutableList()
 
-}
+            // String adjustment - size of the list will be equal to number of options entered
+            trimmedStringsOption =
+                adjustOptions(trimmedStringsOption, numOptions) // 2. fun adjustOptions
+
+            // switch acvtivity managed by other function
+            startPreferenceScreenActivity(numOptions, trimmedStringsOption)
+        }
+    }
 
 
 
     private fun checkInput () {
-        val numOptionsInput = etNumOptions.text.toString()
+        val numOptionsInput = etNumOptions!!.text.toString()
 
         // Check number of Options
         if (numOptionsInput.isNotEmpty() && numOptionsInput.toIntOrNull() !in 2..10) {
-            etNumOptions.setText("")
+            etNumOptions!!.setText("")
             Toast.makeText(this@MainActivity, "Number of Options must be minimum 2 and maximum 10", Toast.LENGTH_SHORT).show()
         }
     }
 
-
-    // Add a Vote - will fill missing string options and remove extra ones
-    //Switch Screens by pressing "Add a Vote - Button"
-    fun onAddVoteButtonClick() { // 1. diese funktion wird aufgerufen , und nacheinander die nächsten ausgeführt innerhalb
-        val numOptions = etNumOptions.text.toString().toIntOrNull() ?: 3
-        var trimmedStringsOption = etVotingOptions.text.toString().split(",").map { it.trim() }.toMutableList()
-
-        // String adjustment - size of the list will be equal to number of options entered
-        trimmedStringsOption = adjustOptions(trimmedStringsOption, numOptions) // 2. fun adjustOptions
-
-        // switch acvtivity managed by other function
-        startPreferenceScreenActivity(numOptions, trimmedStringsOption)
-    }
 
     private fun adjustOptions(options: MutableList<String>, numOptions: Int): MutableList<String> {
         // Ensure that the size of trimmedStringsOption matches numOptions
@@ -76,10 +88,6 @@ class MainActivity : AppCompatActivity() {
         intent.putStringArrayListExtra("trimmedStringsOption", ArrayList(options))
         startActivity(intent)
     }
-
-
-
-
 
 
 
